d853762e3f1eafa2f6cbdbde7183d515872d2ee0
Curious-DevlOper <elham.bolandi@gmai.com>
2023-07-05 23:16:02 +0200

    third assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'],
-  styles: [`
-    h3 {
-      color: darkred;
-    }
-  `]
+  styleUrls: ['./app.component.css'],
+ 
 })
 export class AppComponent {
   title = 'my-second-app';
-  username: string = 'Initial Username';
-  allowReset = true;
-
-  checkInput() {
-    this.allowReset = this.username == '' ?  true : false;
-  }
-
-  restUsername(){
-
-    this.username = '';
-
+  pass: string = ' tuna';
+  isPassHidden = false ;
+  logsNum : number= 0 ;
+  logs: string[] = [];
+  blueBackgroundIndex: number = 4;
+ 
+  togglePass(event: any) {
+    this.isPassHidden = ! this.isPassHidden;
+    this.logsNum ++;
+    const logMessage = 'log results: ' + this.logsNum;
+    console.log(logMessage);
+    this.logs.push(logMessage);
   }
 }
