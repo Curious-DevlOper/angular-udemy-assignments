3f3f8b99d7b3be1e58688f7d843fcbddf7ee4689
Curious-DevlOper <elham.bolandi@gmai.com>
2023-08-01 16:51:50 +0200

    use cusome property binding & HostListener

--- src/app/basic-highlight/better-highlight-directive/better-highlight.directive.ts
+++ src/app/basic-highlight/better-highlight-directive/better-highlight.directive.ts
@@ -1,20 +1,35 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'pink';
 
+  // dynamically set value
+  // The colore we want to set changes depending on some other parameters 
+  //the colors are hard coded. use cusome property binding 
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  
   constructor(private elRef: ElementRef,  private renderer: Renderer2) { }
+
   ngOnInit() {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'pink' );
+    this.backgroundColor = this.defaultColor;
 
   }
   //hover over the text element, and its background color will change
   @HostListener('mouseenter') mouseover(eventDate: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'pink' );
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', this.highlightColor );
+    this.backgroundColor = this.highlightColor ;
+
   }
   @HostListener('mouseleave') mouseleave(eventDate: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent' );
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', this.defaultColor );
+    this.backgroundColor = this.defaultColor;
+
   }
+  
+
 }
