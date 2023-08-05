aa8d1541a7102aa5413130f57805c25ac8ec68b1
Curious-DevlOper <elham.bolandi@gmai.com>
2023-08-05 15:41:25 +0200

    closing Dropdown from anywhere!

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,13 +1,20 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
     selector:  '[appDropdown]'
 })
 export class DropdownDirective {
     //to dinamically attach or detach css class depending on this
-    @HostBinding('class.open') isOpen = false;
-    @HostListener ('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
-    }
+    @HostBinding('class.show') isOpen = false;
+
+    // @HostListener ('click') toggleOpen() {
+    //     this.isOpen = !this.isOpen;
+    // }
+
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+      }
+      constructor(private elRef: ElementRef) {}
+    
 
 }
